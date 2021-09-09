30a3289035311e9a29d7423eba00414e0a7c85e3
cipauuu <citra@mamiteam.com>
2021-09-10 01:22:36 +0700

    update template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
