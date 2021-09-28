973efb43fc488941a2f29782459112b91fbdd0bc
cipauuu <citra@mamiteam.com>
2021-09-28 20:34:04 +0700

    homepage (on-progress)

--- src/pages/RegisterLogin/TopHeader.jsx
+++ src/pages/RegisterLogin/TopHeader.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import logo from "../../img/logo.svg";
 
 const TopHeader = ({ title }) => {
   return (
     <div className="mt-4 mb-5">
-      <img src={logo} alt="" />
+      <img src="assets/logo.svg" alt="" />
       <p className="judul">{title}</p>
       <p className="sub-judul">Masukan detail untuk melanjutkan</p>
     </div>
