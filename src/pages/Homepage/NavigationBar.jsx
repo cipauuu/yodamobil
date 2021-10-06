c65ee2dd13d07e269c3f876553a8a9b786583a96
cipauuu <38646181+cipauuu@users.noreply.github.com>
2021-10-07 02:46:59 +0700

    update navbar hide/show "daftar" button

--- src/pages/Homepage/NavigationBar.jsx
+++ src/pages/Homepage/NavigationBar.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 
 const NavigationBar = () => {
-  // const token = Cookies.get("token");
+  const token = Cookies.get("token");
 
-  // useEffect(() => {
-  //   if((token === "") || (token === undefined) || (token === null)){
-  //     document.getElementById("daftar").style.display = "none";
-  //   }
-  // });
+  useEffect(() => {
+    if(token){
+      document.getElementById("daftar1").remove()
+      document.getElementById("daftar2").remove()
+    }
+  });
 
   return (
     <Navbar bg="white" expand="lg">
@@ -19,7 +20,7 @@ const NavigationBar = () => {
         <Link to="/" className="navbar-brand">
           <img src="assets/logo.svg" alt="" />
         </Link>
-        <Link to="/register" className="nav-daftar2 my-auto mr-3">
+        <Link id="daftar1" to="/register" className="nav-daftar2 my-auto mr-3">
           <img src="assets/ic_nav_daftar.svg" alt="" className="mr-2" />
           Daftar
         </Link>
@@ -48,7 +49,7 @@ const NavigationBar = () => {
                 Mitra Dealer
               </Link>
             </li>
-            <li className="nav-item" id="daftar">
+            <li className="nav-item" id="daftar2">
               <Link to="/register" className="nav-daftar1 my-auto">
                 <img src="assets/ic_nav_daftar.svg" alt="" className="mr-2" />
                 Daftar
